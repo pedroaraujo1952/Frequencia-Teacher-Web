25de473a89699c0e0b58fea6125e9184120e2bdf
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-03-23 21:38:38 -0400

    Add Environment variables

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,14 +1,14 @@
-import firebase from "firebase";
+import * as firebase from "firebase";
 
 const config = {
-  apiKey: "AIzaSyBlQQI3JBcknV3aO26rg2LAlPNehiqocEs",
-  authDomain: "fmm-check-student.firebaseapp.com",
-  databaseURL: "https://fmm-check-student.firebaseio.com",
-  projectId: "fmm-check-student",
-  storageBucket: "fmm-check-student.appspot.com",
-  messagingSenderId: "100699814698",
-  appId: "1:100699814698:web:5cf7d564f5c4f1ab18fc53",
-  measurementId: "G-CSTFMB3P7H"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGEBUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 export const fire = firebase.initializeApp(config);
