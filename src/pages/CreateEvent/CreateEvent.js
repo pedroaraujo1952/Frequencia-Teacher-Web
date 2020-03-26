0c809d8807a54ff573a5b2d37dedf8dd8af11fb8
Lorenzo Windmoller <lorenzomart01@gmail.com>
2020-03-26 13:14:24 -0400

    Fields validation added

--- src/pages/CreateEvent/CreateEvent.js
+++ src/pages/CreateEvent/CreateEvent.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { fire } from "../../config/firebaseConfig";
+import MaskedInput from 'react-text-mask';
 
 import Header from "../../components/CreateEventHeader/Header";
 import Loading from "../../components/Loading/Loading";
@@ -59,15 +60,81 @@ export default class CreateEvent extends Component {
 
   handleAddWord = ev => {
     ev.preventDefault();
-    var word = this.state.word + ', ' + this.state.notifyHour;
-    const { keyWord } = this.state;
-    keyWord.push(word); 
-    this.setState({keyWord})
+    var el = document.getElementsByClassName('warningNotifyHour');
+    var h = parseInt(this.state.notifyHour.substring(0,2));
+    var min = parseInt(this.state.notifyHour.substring(3,5));
+    console.log(h,min)
+    if(
+      this.state.word === '' ||
+      this.state.notifyHour === '' ||
+      this.state.hourBegin === '' ||
+      this.state.minutesBegin === '' ||
+      this.state.hourEnd === '' ||
+      this.state.minutesEnd === ''
+    ) {
+      el[0].style.display = 'block';
+    } else if (
+      h > parseInt(this.state.hourEnd) ||
+      h < parseInt(this.state.hourBegin) ||
+      (h === parseInt(this.state.hourEnd) && min >= parseInt(this.state.minutesEnd)) ||
+      (h === parseInt(this.state.hourBegin) && min <= parseInt(this.state.minutesBegin))
+    ) {
+      el[0].style.display = 'block';
+    } else {
+      el[0].style.display = 'none';
+      var word = this.state.word + ', ' + this.state.notifyHour;
+      const { keyWord } = this.state;
+      keyWord.push(word); 
+      this.setState({keyWord})
+    }
   };
 
   handleChange = ev => {
     this.setState({ [ev.target.name]: ev.target.value });
   };
+
+  verify = ev => {
+    ev.preventDefault();
+    var el = document.getElementsByClassName('warning');
+    var d = parseInt(this.state.date.substring(0,2));
+    var m = parseInt(this.state.date.substring(3,5));
+    var y = parseInt(this.state.date.substring(6,8));
+    if(
+      this.state.name === '' ||
+      this.state.hourBegin === '' ||
+      this.state.minutesBegin === '' ||
+      this.state.hourEnd === '' ||
+      this.state.minutesEnd === '' ||
+      this.state.date === '' ||
+      this.state.keyWord === [] ||
+      this.state.description === ''
+    ) {
+      el[0].style.display = 'block';
+    } else if(
+      parseInt(this.state.hourEnd) >= 24 ||
+      parseInt(this.state.minuteEnd) >= 60 ||
+      parseInt(this.state.hourBegin) >= 24 ||
+      parseInt(this.state.minuteBegin) >= 60
+    ) {
+      el[0].style.display = 'block';
+    } else if(
+      d > 31 ||
+      m > 12 ||
+      y !== 20 
+    ) {
+      el[0].style.display = 'block';
+    } else {
+      el[0].style.display = 'none';
+    } 
+  }
+
+  sendEvent = ev => {
+    ev.preventDefault();
+    /*
+    Send to database
+    */
+    this.setState({backHome: true});
+  }
 
   /*reauthenticate = (email, password) => {
     var user = firebase.auth().currentUser;
@@ -120,34 +187,62 @@ export default class CreateEvent extends Component {
             <div className="newEvent">
                 <div className="title">
                     <h1>Título</h1>
-                    <input type="text" name="title" onChange={this.handleChange}/>
+                    <input type="text" name="name" onChange={this.handleChange}/>
                 </div>
                 <div className="timeBegin">
                     <h1>Horário de Início</h1>
-                    <input type="text" name="hourBegin" onChange={this.handleChange}/>
+                    <MaskedInput
+                      name="hourBegin"
+                      mask={[/[0-9]/,/\d/]}
+                      onChange={this.handleChange}
+                    />
                     <h2 name="h">h</h2>
-                    <input type="text" name="minutesBegin" onChange={this.handleChange}/>
+                    <MaskedInput
+                      name="minutesBegin"
+                      mask={[/[0-9]/,/\d/]}
+                      onChange={this.handleChange}
+                    />
                     <h2 name="min">min</h2>
                 </div>
                 <div className="timeEnd">
                     <h1>Horário de Término</h1>
-                    <input type="text" name="hourEnd" onChange={this.handleChange}/>
+                    <MaskedInput
+                      name="hourEnd"
+                      mask={[/[0-9]/,/\d/]}
+                      onChange={this.handleChange}
+                    />
                     <h2 name="h">h</h2>
-                    <input type="text" name="minutesEnd" onChange={this.handleChange}/>
+                    <MaskedInput
+                      name="minutesEnd"
+                      mask={[/[0-9]/,/\d/]}
+                      onChange={this.handleChange}
+                    />
                     <h2 name="min">min</h2>
                 </div>
                 <div className="date">
                     <h1>Data</h1>
-                    <input type="text" name="date" onChange={this.handleChange}/>
+                    <MaskedInput
+                      name="date"
+                      mask={[/[0-9]/,/\d/,'/',/\d/,/\d/,'/',/\d/,/\d/]}
+                      onChange={this.handleChange}
+                    />
                 </div>
                 <div className="keyWords">
+                    <div className="warningNotifyHour">
+                      <h2 name="msgWarning">*Informe um horário entre o intervalo da aula</h2>
+                    </div>
                     <h1>Palavras-passe</h1>
                     <input type="text" name="word" placeholder="Palavra" onChange={this.handleChange}/>
-                    <input type="text" name="notifyHour" placeholder="Horário da notificação" onChange={this.handleChange}/>
+                    <MaskedInput
+                      name="notifyHour"
+                      placeholder="Horário da notificação"
+                      mask={[/[0-9]/,/\d/,'h',/\d/,/\d/,'min']}
+                      onChange={this.handleChange}
+                    />
                     <h2 name="button" onClick={this.handleAddWord}>+</h2>
                     <div className="words">
                         {this.state.keyWord.map((k) => (
-                            <h2>{k}</h2>
+                            <h2 key={k}>{k}</h2>
                         ))}
                     </div>
                 </div>
@@ -156,21 +251,15 @@ export default class CreateEvent extends Component {
                     <input type="text" name="description" onChange={this.handleChange}/>
                 </div>
                 <div className="saveButton">
-                    <button
-                        /*onClick={ev => {
-                          ev.preventDefault();
-                        }}*/
-                    >
+                    <button onClick={this.verify}>
                     <h2>Salvar</h2> 
                     </button>
                 </div>
+                <div className="warning">
+                  <h2 name="msgWarning">*Preencha todos os campos corretamente</h2>
+                </div>
                 <div className="cancelButton">
-                    <button
-                        onClick={ev => {
-                          ev.preventDefault();
-                          this.setState({backHome: true});
-                        }}
-                    >
+                    <button onClick={this.sendEvent}>
                     <h2>Cancelar</h2> 
                     </button>
                 </div>
