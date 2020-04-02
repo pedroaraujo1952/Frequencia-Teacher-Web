790dbc2937f72c92ca7370281e26fcfba00e9896
Lorenzo Windmoller <lorenzomart01@gmail.com>
2020-04-01 23:58:05 -0400

    Keeping keywords after editing an event

--- src/controllers/EventController.js
+++ src/controllers/EventController.js
@@ -72,11 +72,29 @@ export async function deleteEvent(event, className) {
 }
 
 export async function updateEvent(state) {
+  var keysAfter = 0;
+  if(state.keyWord[0] !== "")keysAfter++;
+  if(state.keyWord[1] !== "")keysAfter++;
+  if(state.keyWord[2] !== "")keysAfter++;
+  if(keysAfter !== 0){
+    state.key = {
+      key1: {
+        key: state.keyWord[0],
+        time: state.notifTime[0]
+      },
+      key2: {
+        key: state.keyWord[1],
+        time: state.notifTime[1]
+      },
+      key3: {
+        key: state.keyWord[2],
+        time: state.notifTime[2]
+      }
+    }
+  }
   return new Promise(async (resolve, reject) => {
     try {
       var uid = fire.auth().currentUser.uid;
-      console.log(state)
-
       var data = {
         title: state.name,
         subject: await User.getSubject(uid),
@@ -87,20 +105,7 @@ export async function updateEvent(state) {
         date: state.date,
         description: state.description,
         end: state.hourEnd + "h" + state.minutesEnd,
-        keys: {
-          key1: {
-            key: state.keyWord[0],
-            time: state.notifTime[0]
-          },
-          key2: {
-            key: state.keyWord[1],
-            time: state.notifTime[1]
-          },
-          key3: {
-            key: state.keyWord[2],
-            time: state.notifTime[2]
-          }
-        },
+        keys: state.key,
         students: state.students
       };
 
