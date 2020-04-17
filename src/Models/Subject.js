1c65ac5b27422f06147ad66dba8bb234a36a62e0
Ph-FMM <pedroaraujo1952@hotmail.com>
2020-04-17 01:05:56 -0400

    Bug Fix

---
+++ src/Models/Subject.js
@@ -0,0 +1,108 @@
+const subject = [
+  {
+    name: "ACST",
+    label: "ACST",
+  },
+  {
+    name: "APE",
+    label: "APE",
+  },
+  {
+    name: "API",
+    label: "API",
+  },
+  {
+    name: "APM",
+    label: "APM",
+  },
+  {
+    name: "APH",
+    label: "Automação, Pneumática e Hidráulica",
+  },
+  {
+    name: "CLP",
+    label: "Controles Lógicos Programáveis",
+  },
+  {
+    name: "Arte",
+    label: "Arte",
+  },
+  {
+    name: "Biologia",
+    label: "Biologia",
+  },
+  {
+    name: "Ed. Física",
+    label: "Ed. Física",
+  },
+  {
+    name: "Eletricidade I",
+    label: "Eletricidade I",
+  },
+  {
+    name: "E. Industrial",
+    label: "E. Industrial",
+  },
+  {
+    name: "Filosofia",
+    label: "Filosofia",
+  },
+  {
+    name: "Física",
+    label: "Física",
+  },
+  {
+    name: "Geografia",
+    label: "Geografia",
+  },
+  {
+    name: "História",
+    label: "História",
+  },
+  {
+    name: "L. Inglesa",
+    label: "L. Inglesa",
+  },
+  {
+    name: "L. Portuguesa",
+    label: "L. Portuguesa",
+  },
+  {
+    name: "LTP",
+    label: "LTP",
+  },
+  {
+    name: "LPIII",
+    label: "LP III",
+  },
+  {
+    name: "WEB",
+    label: "WEB",
+  },
+  {
+    name: "Matemática",
+    label: "Matemática",
+  },
+  {
+    name: "Mobile",
+    label: "Mobile",
+  },
+  {
+    name: "Mobile",
+    label: "Mobile",
+  },
+  {
+    name: "Micro",
+    label: "Microcontroladores",
+  },
+  {
+    name: "Sociologia",
+    label: "Sociologia",
+  },
+  {
+    name: "UX",
+    label: "UX",
+  },
+];
+
+export default subject;
---
+++ src/components/MultipleSelect/MultipleSelect.js
@@ -0,0 +1,59 @@
+import React from "react";
+import { makeStyles, withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import InputBase from "@material-ui/core/InputBase";
+
+import subjects from "../../models/Subject";
+import { Checkbox, ListItemText } from "@material-ui/core";
+
+const BootstrapInput = withStyles((theme) => ({
+  input: {
+    borderRadius: 8,
+    position: "relative",
+    backgroundColor: theme.palette.background.paper,
+    fontSize: 22,
+    padding: "11px 26px 10px 12px",
+    transition: theme.transitions.create(["border-color", "box-shadow"]),
+    "&:focus": {
+      borderRadius: 8,
+      borderColor: "#80bdff",
+      backgroundColor: theme.palette.background.paper,
+    },
+  },
+}))(InputBase);
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    minWidth: "100%",
+  },
+}));
+
+export default function MultipleSelect({ onChange, value, name }) {
+  const classes = useStyles();
+  return (
+    <div>
+      <FormControl className={classes.formControl}>
+        <Select
+          labelId="demo-customized-select-label"
+          id="demo-customized-select"
+          className="selectCustom"
+          name={name}
+          value={value}
+          multiple
+          onChange={onChange}
+          renderValue={(selected) => selected.join(", ")}
+          input={<BootstrapInput />}
+        >
+          {subjects.map((subject) => (
+            <MenuItem key={subject.name} value={subject.name}>
+              <Checkbox checked={value.indexOf(subject.name) > -1} />
+              <ListItemText primary={subject.label} />
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
+  );
+}
